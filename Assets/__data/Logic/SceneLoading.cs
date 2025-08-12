b45b471e7bb83b9916a58ec51a05458d3467fcea
AnvarRocket888 <anvarsalakhov@syndicate-g.com>
2025-08-12 11:19:16 +0300

    att

--- Assets/__data/Logic/SceneLoading.cs
+++ Assets/__data/Logic/SceneLoading.cs
@@ -1,11 +1,16 @@
 using System.Collections;
+using System.Net;
+using Unity.Advertisement.IosSupport;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class SceneLoading : MonoBehaviour
 {
     IEnumerator Start()
     {
+        if (ATTrackingStatusBinding.GetAuthorizationTrackingStatus() == ATTrackingStatusBinding.AuthorizationTrackingStatus.NOT_DETERMINED)
+        {
+            ATTrackingStatusBinding.RequestAuthorizationTracking();
+        }
         yield return new WaitForSeconds(5f);
         SceneManager.LoadScene(1);
     }
